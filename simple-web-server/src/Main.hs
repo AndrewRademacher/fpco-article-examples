b105be24caaa9cf3867a3793b67c4e287fc83072
AndrewRademacher <andrewrademacher@gmail.com>
2016-12-18 15:50:23 -0600

    Ran thrugh example.

--- simple-web-server/src/Main.hs
+++ simple-web-server/src/Main.hs
@@ -2,11 +2,33 @@
 
 module Main where
 
+import Data.Text.Lazy
 import Web.Scotty
 
 main :: IO ()
 main = scotty 3000 $ do
-  get "/message" $ html "Hello World!"
+  get "/add/:a/:b" $ binaryMathRoute (+)
+  get "/sub/:a/:b" $ binaryMathRoute (-)
+  get "/mul/:a/:b" $ binaryMathRoute (*)
+  get "/div/:a/:b" $ binaryMathRoute div
+  get "/rem/:a/:b" $ binaryMathRoute rem
+
+  get "/float_div/:a/:b" $ binaryMathRoute (/)
+
+renderResult :: (Show a) => a -> ActionM ()
+renderResult x = html $ mconcat [ "<h1>", pack (show x), "</h1>" ]
+
+performBinaryOperation :: (Read a) => (a -> a -> a) -> ActionM a
+performBinaryOperation op = do
+  a <- param "a"
+  b <- param "b"
+  return $ (read a) `op` (read b)
+
+binaryMathRoute :: (Read a, Show a) => (a -> a -> a) -> ActionM ()
+binaryMathRoute op = do
+  x <- performBinaryOperation op
+  renderResult x
+
 
 
 -- {-# LANGUAGE OverloadedStrings #-}
